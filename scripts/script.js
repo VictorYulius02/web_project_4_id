bb960b63571fa8e8472ba2e2f51a34d04ad45581
Victor Yulius <vhome02@gmail.com>
2023-06-03 11:01:44 +0700

    hotfix: everything needs correcting

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,29 @@
-const formElement = document.querySelector('.page');
-const editBtn = formElement.querySelector('.profile-info__edit-button');
-//const addBtn = formElement.querySelector('.profile__add-button');
-const nameProfile = formElement.querySelector('.profile-info__name');
-const jobProfile = formElement.querySelector('.profile-info__job');
-const editForm = formElement.querySelector('.edit-form');
-const nameInput = formElement.querySelector('.edit-form__name');
-const jobInput = formElement.querySelector('.edit-form__job');
-const editFormCloseButton = formElement.querySelector('.edit-form__close-button');
-const editFormSaveButton = formElement.querySelector('.edit-form__save-button');
+const editBtn = document.querySelector('.profile-info__edit-button');
+const nameProfile = document.querySelector('.profile-info__name');
+const jobProfile = document.querySelector('.profile-info__job');
+const editForm = document.querySelector('.edit-popup');
+const nameInput = document.querySelector('input[name="nameInput"]');
+const jobInput = document.querySelector('input[name="jobInput"]');
+const editFormCloseButton = document.querySelector('.edit-popup__close-button');
+const editFormSaveButton = document.querySelector('.edit-popup__save-button');
 
 function handleEditFormActive() {
-  editForm.classList.add('edit-form-active');
-  nameInput.setAttribute('placeholder' , nameProfile.textContent);
-  jobInput.setAttribute('placeholder' , jobProfile.textContent);
+  editForm.classList.add('edit-popup_active');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
 
 function handleCloseEditForm() {
-  editForm.classList.remove('edit-form-active');
+  editForm.classList.remove('edit-popup_active');
 }
 
-function handleSaveEditForm() {
+function handleSaveEditForm(event) {
+  event.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  editForm.classList.remove('edit-form-active');
+  handleSaveEditForm();
 }
 
 editBtn.addEventListener('click' , handleEditFormActive);
 editFormCloseButton.addEventListener('click' , handleCloseEditForm);
-editFormSaveButton.addEventListener('click' , handleSaveEditForm);
+editForm.addEventListener('submit' , handleSaveEditForm)
