e8ab4b9c53bcb3a33335431116a7cc2a1535d10b
Victor Yulius <vhome02@gmail.com>
2023-06-21 10:48:05 +0700

    Before Adding transition

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 const editBtn = document.querySelector('.profile-info__edit-button');
 const nameProfile = document.querySelector('.profile-info__name');
 const jobProfile = document.querySelector('.profile-info__job');
+
 const editPopup = document.querySelector('.edit-popup');
 const nameInput = document.querySelector('input[name="nameInput"]');
 const jobInput = document.querySelector('input[name="jobInput"]');
 const editPopupCloseButton = document.querySelector('.edit-popup__close-button');
+
+const likeBtn = document.querySelectorAll('.element__like-button');
 
 const addBtn = document.querySelector('.profile__add-button');
 const addPopup = document.querySelector('.add-popup');
@@ -12,6 +15,8 @@ const titleInput = document.querySelector('input[name="titleInput"]');
 const webAddressInput = document.querySelector('input[name="webAddressInput"]');
 const addPopupCloseButton = document.querySelector('.add-popup__close-button');
 const saveBtn = document.querySelector('.add-popup__save-button');
+const titleErrorMsg = document.querySelector('p[name="titleErrorMsg"]');
+const webAddressErrorMsg = document.querySelector('p[name="webAddressErrorMsg"]');
 
 const elementTemplate = document.querySelector('.element-template');
 const elements = document.querySelector('.elements');
@@ -45,27 +50,45 @@ const initialCards = [
   }
 ];
 
-/*for (let i = 0; i <= 5 ; i++) {
-  const initialTemplate = elementTemplate.content.cloneNode(true);
-  const elementImage = initialTemplate.querySelector('.element__image');
-  const elementLocation = initialTemplate.querySelector('.element__location');
-  
-  elementImage.src = initialCards[i].link;
-  elementLocation.textContent = initialCards[i].name;
-  
-  elements.append(initialTemplate);
-}*/
+function createTemplate() {
+  while (elements.firstChild) {
+    elements.firstChild.remove();
+  }
 
-initialCards.forEach((data) => {
-  const initialTemplate = elementTemplate.content.cloneNode(true);
-  const elementImage = initialTemplate.querySelector('.element__image');
-  const elementLocation = initialTemplate.querySelector('.element__location');
-  const likeButton = initialTemplate.querySelector('.element__like-button');
-  
-  elementImage.src = data.link;
-  elementLocation.textContent = data.name;
-  
-  elements.append(initialTemplate);
+  initialCards.forEach((data) => {
+    const initialTemplate = elementTemplate.content.cloneNode(true);
+    const elementImage = initialTemplate.querySelector('.element__image');
+    const elementLocation = initialTemplate.querySelector('.element__location');
+   
+    elementImage.src = data.link;
+    elementLocation.textContent = data.name;
+    
+    elements.append(initialTemplate);
+  });
+}
+
+createTemplate();
+
+function handleDeleteElement(event) {
+  const deleteBtn = event.target;
+  const element = deleteBtn.closest('.element');
+
+  const index = Array.from(elements.children).indexOf(element);
+
+  element.remove();
+  initialCards.splice(index, 1);
+}
+
+elements.addEventListener('click', function(event) {
+  if (event.target.classList.contains('element__delete')) {
+    handleDeleteElement(event);
+  }
+});
+
+elements.addEventListener('click', function(event) {
+  if (event.target.classList.contains('element__like-button')) {
+    event.target.classList.toggle('element__like-button_active');
+  }
 });
 
 function handleEditFormActive() {
@@ -89,50 +112,87 @@ editBtn.addEventListener('click' , handleEditFormActive);
 editPopupCloseButton.addEventListener('click' , handleCloseEditForm);
 editPopup.addEventListener('submit' , handleSaveEditForm);
 
-function saveBtnDisabled() {
+/*function saveBtnDisabled() {
   saveBtn.classList.add('add-popup__save-button_disabled');
   saveBtn.setAttribute('disabled' , 'true');
+}*/
+
+function enableSaveBtn() {
+  if (titleInput.value !== '' || webAddressInput.value !== '') {
+
+    titleInput.style.opacity = "1";
+    webAddressInput.style.opacity = "1";
+
+    saveBtn.disabled = false;
+    saveBtn.classList.remove('add-popup__save-button_disabled');
+  }
 }
 
 function handleAddFormActive() {
   addPopup.classList.add('add-popup_active');
   titleInput.setAttribute("placeholder" , "Judul");
   webAddressInput.setAttribute("placeholder" , "URL Gambar");
-  saveBtnDisabled();
+  
+  titleInput.addEventListener('input' , enableSaveBtn);
+  webAddressInput.addEventListener('input' , enableSaveBtn);
 
-  if (titleInput.value !== "" || webAddressInput.value !== "") {
-    titleInput.opacity = "1";
-    webAddressInput.opacity = "1";
-    saveBtn.classList.remove('add-popup__save-button_disabled');
-  }
+  enableSaveBtn();
 }
 
 function handleCloseAddForm() {
   addPopup.classList.remove('add-popup_active');
 }
 
-function handleSaveAddForm (event) {
+function handleSaveAddForm(event) {
   event.preventDefault();
-  const initialTemplate = elementTemplate.content.cloneNode(true);
-  const elementImage = initialTemplate.querySelector('.element__image');
-  const elementLocation = initialTemplate.querySelector('.element__location');
-  const likeButton = initialTemplate.querySelector('.element__like-button');
 
-  elementImage.src = webAddressInput.value;
-  elementLocation.textContent = titleInput.value;
+  const titleValue = titleInput.value;
+  const webAddressValue = webAddressInput.value;
 
-  elements.append(initialTemplate);
+  if (titleValue.trim() !== '') {
+    titleErrorMsg.style.display ='none';
+  }
+
+  if (webAddressValue.trim() !== '') {
+    webAddressErrorMsg.style.display ='none';
+  }
+
+  if (titleValue.trim() === '') {
+    titleErrorMsg.style.display = 'block';
+    return;
+  }
+
+  if (webAddressValue.trim() === '') {
+    webAddressErrorMsg.style.display = 'block';
+    return;
+  }
+
+  const newCard = {
+    name: titleValue,
+    link: webAddressValue
+  };
+
+  initialCards.push(newCard);
+  createTemplate();
   handleCloseAddForm();
+
+  // Reset the form fields
+  titleInput.value = '';
+  webAddressInput.value = '';
+  titleErrorMsg.style.display = 'none';
+  webAddressErrorMsg.style.display = 'none';
 }
 
-input.addEventListener("keypress" , event => {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    addPopup.click();
-  }
-})
+addPopup.addEventListener('submit', handleSaveAddForm);
 
 addBtn.addEventListener('click' , handleAddFormActive);
 addPopupCloseButton.addEventListener('click' , handleCloseAddForm);
 addPopup.addEventListener('submit' , handleSaveAddForm);
+/*addPopup.addEventListener('submit' , createTemplate);*/
 
+input.addEventListener("keypress" , event => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addPopup.submit();
+  }
+})
